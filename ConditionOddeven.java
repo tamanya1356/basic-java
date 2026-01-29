28044939dd673289fb2525660ba342eb9ea28a04
Tamanya <tamanyaghoshal7@gmail.com>
2026-01-29 10:32:24 +0530

    4th java code

---
+++ ConditionOddeven.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class ConditionOddeven {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);{
+            int x=sc.nextInt();
+            if (x%2==0){
+                System.out.println("even");
+            } else{
+                System.out.println("odd");
+        
+            }
+        }
+    }
+}
