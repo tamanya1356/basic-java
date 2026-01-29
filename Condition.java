4762189626d40b46f11c17c82a5695f4e20bf321
Tamanya <tamanyaghoshal7@gmail.com>
2026-01-29 10:31:01 +0530

    2nd java code

---
+++ Condition.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class Condition {
+    public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   int age=sc.nextInt(); // input nilam detatypes (int) and variable(age)
+    
+   if(age>18){
+  System.out.println("adult");
+
+   }
+  else{
+    System.out.println("not adult");
+  }
+
+
+    }
+
+
+
+    }
+    
+
